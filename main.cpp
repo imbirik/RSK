6bc40f60dffa2abf0c18d072e251fce6febafeff
imbirik <32593555+imbirik@users.noreply.github.com>
2019-09-11 16:01:04 +0300

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -9,31 +9,126 @@
 #include <map>
 #include <ctime>
 
-using namespace std; 
+using namespace std;
 
-const long MAXN = 1e4;
+const size_t MAXN = (size_t)1e4;                 //size of YT
+const double EPS = 1e-8;
 long long factorial[20];
 //const unsigned int N = 5;
-long double branch_prob[MAXN];
+//long double branch_prob[MAXN];
 
 
 int main() {
-	//freopen("out.txt", "w", stdout);
-	freopen("err.txt", "w", stderr);
+	freopen("out.txt", "w", stdout);
+	//freopen("err.txt", "w", stderr);
+	cout.precision(6);
 
-	long b[10];
-	for (int i = 0; i < 10; ++i)
+	size_t N = MAXN;
+	double *r = new double[N];
+	rand_arr(r, N);
+	PQ_tableaux<double> PQ;
+	PQ.push(r, N);
+	double *points = new double[10];
+	for (size_t i = 0; i < 10; ++i)
 	{
-		b[i] = i;
-		cout << i << ' ';
+		points[i] = ((double)i + 1) / 10;
 	}
-	cout << "\n";
-	rand_permute_self_inverse<long>(b, 10);
-	for (int i = 0; i < 10; ++i)
+	const size_t M = (size_t)1e5;
+	pair<double, double> coord[10][M / 100];
+	PQ.push(points, 10);
+
+	for (size_t i = 0; i < M; ++i)
 	{
-		cout << b[i] << ' ';
+		PQ.push(rand01());
+		if (i % 100 == 0)
+		{
+			for (size_t j = 0; j < 10; ++j)
+			{
+				coord[j][i / 100] = PQ.find(points[j]);
+			}
+		}
 	}
-	cout << "\n";
+
+	for (size_t i = 0; i < M / 100; ++i)
+	{
+		for (size_t j = 0; j < 10; ++j)
+		{
+			cout << coord[j][i].first << ' ' << coord[j][i].second << ' ';
+		}
+		cout << "\n";
+	}
+
+	//system("pause");
+	/*
+	double add = 1 / (double)MAXN;
+	double r = 0;
+	for (size_t i = 0; i < MAXN; ++i)
+	{
+		ra[i] = r;
+		r += add;
+	}
+
+	rand_permute_self_inverse(ra, MAXN);
+	*/
+	/*
+	const long MAN = (long)2e2;
+	long *func = new long[MAN];
+	long *height = new long[MAN];
+	long *length = new long[MAN];
+	for (size_t MAXN = 1; MAXN < MAN; ++MAXN)
+	{
+		cerr << MAXN << "\n";
+		double *ra = 0;
+		ra = new double[MAXN];
+		rand_arr(ra, MAXN);
+		PQ_tableaux<double> PQ;
+		PQ.push(ra, MAXN);
+		height[MAXN] = PQ.height();
+		length[MAXN] = PQ.length();
+
+		double *max_ra = new double[MAXN];
+		for (size_t cnt = 0; cnt * cnt < 16 * MAXN; ++cnt)
+		{
+			for (size_t i = 0; i < MAXN; ++i)
+			{
+				double tmp = 0;
+				for (size_t j = 0; j < i; ++j)
+				{
+					tmp = max(tmp, (ra[j] < ra[i]) ? ra[j] : 0);
+				}
+				max_ra[i] = tmp;
+			}
+			PQ_tableaux<double> PQ_max;
+			PQ_max.push(max_ra, MAXN);
+			if (PQ_max.height() == 1)
+			{
+				func[MAXN] = cnt;
+				break;
+			}
+			for (size_t i = 0; i < MAXN; ++i)
+			{
+				ra[i] = max_ra[i];
+			}
+		}
+	}
+
+	for (size_t i = 0; i < MAN; ++i)
+	{
+		cout << i << ' ' << func[i] << ' ' << length[i] << ' ' << height[i] << "\n";
+	}
+	*/
+	/*
+	PQ.showP();
+	cout << "\n\n";
+	PQ.truncate(max_ra[9], ra[9]);
+	PQ.showP();
+	cout << "\n\n";
+	PQ.truncate(max_ra[89], ra[89]);
+	PQ.showP();
+	cout << "\n\n";
+	*/
+
+
 
 	/*
 	int n = 6000;
@@ -51,7 +146,7 @@ int main() {
 	factorial[0] = 1;                                                //Trying to count some statistics on small sizes of YT
 	for (int i = 1; i < 20; ++i)                                     //Firstly, some precounting
 	{
-		factorial[i] = factorial[i - 1] * i;                         
+		factorial[i] = factorial[i - 1] * i;
 	}
 
 	for (int N = 1; N < 5; ++N)
@@ -62,7 +157,7 @@ int main() {
 		cout << "random vector: " << vec << "\n";
 		vector<int> permutation(N);
 		map<vector<int>, vector<double>> identity_points;                   //Map contains info about each type of YT
-		for (int i = 0; i < N; ++i)                                      //For each type: an array with numbers of 
+		for (int i = 0; i < N; ++i)                                      //For each type: an array with numbers of
 		{
 			permutation[i] = i;                                          //the permutation number which contains
 		}                                                                //this number of identity points
@@ -97,7 +192,7 @@ int main() {
 		cout << "id_points statistics for all permutation: " << measure << "\n" << "\n";
 	}
 	*/
-	system("pause");
+	//system("pause");
 	return 0;
 	/*
 	PQ_tableaux PQ;
@@ -112,4 +207,4 @@ int main() {
 		cout << per[i] <<" ";
 	return 0;
 	*/
-}		
+}
